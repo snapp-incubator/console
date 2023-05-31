6fd8512f5d0ebe321ebd784bdb35b3f9e859fbf4
Mohammad Yosefpor <myusefpur@gmail.com>
2023-05-31 17:51:25 +0330

    rev file names

--- frontend/@types/console/VictoryPortal.d.ts
+++ frontend/@types/console/VictoryPortal.d.ts
@@ -3,7 +3,7 @@ import { VictoryPortal } from 'victory-core';
 
 /**
  * Added custom type definitions as victory-core uses a synthetic default import for React
- * which is not compatible with the `./snapp-frontend` tsconfig
+ * which is not compatible with the `./frontend` tsconfig
  **/
 declare module 'victory-core' {
   export declare class VictoryPortal extends React.Component<VictoryPortalProps> {}
